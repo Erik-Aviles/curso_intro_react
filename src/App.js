838bdf36a62c551117b5782540b519dd394984f7
Erika <erik8822@hotmail.com>
2022-09-08 08:47:59 -0500

    Agregando componentes en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import {CreateTodoButtom} from "./CreateTodoButtom.js";
+
+const todos = [
+  {text: 'Aprender ingles intermedio', completed: false},
+  {text: 'Aprender mas sobre css', completed: false},
+  {text: 'Aprender lenguajes de progracion ', completed: false},
+  {text: 'Aprender git y github', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+      <TodoCounter />    
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+            <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButtom />      
+   </React.Fragment>
   );
 }
 
